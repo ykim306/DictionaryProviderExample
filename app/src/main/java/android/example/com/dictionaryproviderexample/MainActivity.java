ec51876af895b942d8f583af3eb09a62fd505c24
unknown <ykim306@gmail.com>
2018-07-22 19:21:33 -0400

    1st attempt at solution

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -20,7 +20,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.support.v4.widget.SimpleCursorAdapter;
 
@@ -28,7 +28,7 @@ import android.support.v4.widget.SimpleCursorAdapter;
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
  * to show an example of accessing the {@link Words} list via its' Content Provider.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +44,18 @@ public class MainActivity extends ActionBarActivity {
         // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
-        // -- YOUR CODE BELOW HERE -- //
+//        String[] fromColumns = {ContactsContract.Data.DISPLAY_NAME,
+//                ContactsContract.CommonDataKinds.Phone.NUMBER};
+//        int[] toViews = {R.id.display_name, R.id.phone_number};
+
+        String[] fromColumns = {Words.WORD, Words.FREQUENCY};
+        int[] toView = {R.id.text1, R.id.text2};
 
         // Set the Adapter to fill the standard two_line_list_item layout with data from the Cursor.
-        SimpleCursorAdapter adapter = null;
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this
+                , android.R.layout.two_line_list_item, cursor, fromColumns, toView, 0);
 
         // Don't forget to attach the adapter to the ListView
+        dictListView.setAdapter(adapter);
     }
 }
