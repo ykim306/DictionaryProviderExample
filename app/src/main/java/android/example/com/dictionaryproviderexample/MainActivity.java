ade7664380cafe05075617f63801cad2bcdaf5c6
unknown <ykim306@gmail.com>
2018-07-22 17:44:02 -0400

    my solution along with gradle upgrade for build fix

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -20,14 +20,14 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
  * to show an example of accessing the {@link Words} list via its' Content Provider.
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +45,23 @@ public class MainActivity extends ActionBarActivity {
         
         // Surround the cursor in a try statement so that the finally block will eventually execute
         try {
-            dictTextView.setText("The UserDictionary contains ");
-            // -- YOUR CODE BELOW HERE -- //
+            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words\n");
+            dictTextView.append("COLUMNS: " + Words._ID + " - " + Words.FREQUENCY + " - " + Words.WORD);
 
             // Get the index of the column containing the actual words, using
             // UserDictionary.Words.WORD, which is the header of the word column.
+            int idColumn = cursor.getColumnIndex(Words._ID);
+            int frequencyColumn = cursor.getColumnIndex(Words.FREQUENCY);
             int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
 
             // Iterates through all returned rows in the cursor.
             while (cursor.moveToNext()) {
                 // Use that index to extract the String value of the word
                 // at the current row the cursor is on.
+                String id = cursor.getString(idColumn);
+                String frequency = cursor.getString(frequencyColumn);
                 String word = cursor.getString(wordColumn);
-                dictTextView.append(("\n" + word));
+                dictTextView.append(("\n" + id + " - " + frequency + " - " + word));
             }
         } finally {
             // Always close your cursor to avoid memory leaks
